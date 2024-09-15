b90b8d6d30c693ea3df4215ea79594b375b382d5
Vinnie <pablovasta1995@gmail.com>
2024-09-16 00:57:00 +0200

    deploy7

--- Marvel Project/src/App.jsx
+++ Marvel Project/src/App.jsx
@@ -3,28 +3,26 @@ import HeaderComponent from './components/HeaderComponent/HeaderComponent'
 import MainComponent from './components/MainComponent/MainComponent'
 import store from './core/redux/store/store'
 import { Provider } from 'react-redux'
-import { BrowserRouter, Routes, Route, Router } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import SelectionMenuComponent from './components/SelectionMenuComponent/SelectionMenuComponent'
 import CardDetailsComponent from './components/CardDetailsComponent/CardDetailsComponent'
 function App() {
   return (
     <Provider store={store}>
-      <Router basename='/Personal-Projects'>
-        <BrowserRouter>
-          <div className='app'>
-            <HeaderComponent/>
-              <Routes>
-                <Route path="/" element={<SelectionMenuComponent section="selectionMenu"/>} />
-                <Route path="/characters" element={<MainComponent section="characters" />}/>
-                <Route path="/characters/:id" element={<CardDetailsComponent section="characters" />}/>
-                <Route path="/comics" element={<MainComponent section="comics" />} />
-                <Route path="/comics/:id" element={<CardDetailsComponent section="comics" />} />
-                <Route path="/series" element={<MainComponent section="series" />} />
-                <Route path="/series/:id" element={<CardDetailsComponent section="series" />} />
-              </Routes>
-          </div>
-        </BrowserRouter>
-      </Router>
+      <BrowserRouter>
+        <div className='app'>
+          <HeaderComponent/>
+            <Routes>
+              <Route path="/" element={<SelectionMenuComponent section="selectionMenu"/>} />
+              <Route path="/characters" element={<MainComponent section="characters" />}/>
+              <Route path="/characters/:id" element={<CardDetailsComponent section="characters" />}/>
+              <Route path="/comics" element={<MainComponent section="comics" />} />
+              <Route path="/comics/:id" element={<CardDetailsComponent section="comics" />} />
+              <Route path="/series" element={<MainComponent section="series" />} />
+              <Route path="/series/:id" element={<CardDetailsComponent section="series" />} />
+            </Routes>
+        </div>
+      </BrowserRouter>
     </Provider>
     
   )
